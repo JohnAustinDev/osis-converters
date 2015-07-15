4c5e4cbe71084739d0f481271d7d954746caada1
John Austin <gpl.programs.info@gmail.com>
2015-07-15 13:06:02 +0600

    Added xorg and openbox installation to Vagrant VM for Calibre

--- VagrantProvision.sh
+++ VagrantProvision.sh
@@ -31,6 +31,7 @@ sudo cpanm HTML::Entities
 
 # Calibre
 if [ ! `which calibre` ]; then
+  sudo apt-get install -y xorg openbox
   sudo apt-get install xdg-utils imagemagick python-imaging python-mechanize python-lxml python-dateutil python-cssutils python-beautifulsoup python-dnspython python-poppler libpodofo-utils libwmf-bin python-chm
   wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
   calibre-customize –b /vagrant/eBooks/OSIS-Input
