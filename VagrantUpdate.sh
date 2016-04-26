f691b2a0ad7c1a8e207b01683ede0af689c219fd
John Austin <gpl.programs.info@gmail.com>
2016-04-26 12:02:18 +0600

    VagrantUpdate.sh will now also update osis-converters as well as the VM

--- VagrantUpdate.sh
+++ VagrantUpdate.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
+# This script updates osis-converters and its Virtual Machine
+
+# Make sure we're in osis-converters root dir
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+# Stash any local changes
+git stash
+echo .
+echo ANY LOCAL CHANGES TO OSIS-CONVERTERS HAVE BEEN STASHED!!
+echo TO RE-APPLY ANY CHANGES YOU MADE, USE: git stash apply
+echo .
+
+# Pull any remote updates
+git pull
+
+# VM must be running before ssh will work
+vagrant up
+
+# Running VagrantProvision.sh on the VM will update the VM's installed software
 vagrant ssh -c "bash /vagrant/VagrantProvision.sh"
