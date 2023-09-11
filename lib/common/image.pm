f82d3344ba4edb89e1ee6c2ca0320fa6d66cb225
Vagrant User <vm@vagrant.net>
2023-09-11 09:42:48 -0700

    Fixed false cover image error introduced with recent commit

--- lib/common/image.pm
+++ lib/common/image.pm
@@ -371,14 +371,15 @@ sub findCover {
     my @fs = readdir(EBD);
     closedir(EBD);
     foreach my $f (@fs) {
+      my $ext = $f =~ /\.([^\.]+)$/i ? $1 : '';
       my $fscope = $f;
       my $m = $mod.'_';
-      if ($fscope =~ s/^($m)?(.*?)\.png$/$2/i) {
-        &Error("Cover image must be jpg, is: $f");
-      }
       if ($fscope !~ s/^($m)?(.*?)\.jpg$/$2/i) {next;}
       $fscope =~ s/_comp$//; # remove any composite marker to get scope
-      if ($scope eq $fscope) {return "$dir/$f";}
+      if ($scope eq $fscope) {
+        if ($ext !~ /jpg/i) {&Error("Cover image must be jpg, is: $f");}
+        return "$dir/$f";
+      }
     }
   }
   
